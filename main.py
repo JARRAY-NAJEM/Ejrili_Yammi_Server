7d1b7ed61f043ef7501105039a5ebf3980a867c0
Najmeddine <najmejarray@gmail.com>
2023-04-23 23:51:51 +0100

    deployApp puts

--- main.py
+++ main.py
@@ -197,30 +197,21 @@ def update_user(email):
         conn = get_db_connection()
         cur = conn.cursor(cursor_factory=extras.RealDictCursor)
         new_user = request.get_json()
-        firstname = new_user['firstname']
-        name = new_user['name']
-        username = new_user['username']
         email = new_user['email']
-        password = new_user['password']
-        phone = new_user['phone']
-        gradient = new_user['gradient']
-        relationship = new_user['relationship']
-        contact1 = new_user['contact1']
-        contact2 = new_user['contact2']
-        information = new_user['information']
-        medications = new_user['medications']
-        allergies = new_user['allergies']
-        cur.execute("UPDATE account_user SET firstname=%s,name=%s,username=%s,email=%s,phone=%s,password=%s,gradient=%s,relationship=%s,contact1=%s,contact2=%s,information=%s,medications=%s,allergies=%s WHERE id = %s RETURNING *",
-                    (firstname, name, username, email, phone, password, gradient, relationship, contact1, contact2, information, medications, allergies, id))
+        cur.execute(
+            "UPDATE account_user SET firstname=%s,name=%s,username=%s,email=%s,phone=%s,password=%s,gradient=%s,relationship=%s,contact1=%s,contact2=%s,information=%s,medications=%s,allergies=%s WHERE id = %s RETURNING *",
+            (new_user['firstname'], new_user['name'], new_user['username'], new_user['email'], new_user['phone'], new_user['password'], new_user['gradient'], new_user['relationship'], new_user['contact1'], new_user['contact2'], new_user['information'], new_user['medications'], new_user['allergies'], id)
+        )
         updated_user = cur.fetchone()
         conn.commit()
+        if not updated_user:
+            raise HTTPNotFound(description='User not found')
+        return jsonify(updated_user), 200
+    except Exception as e:
+        raise HTTPInternalServerError(description=str(e))
+    finally:
         cur.close()
         conn.close()
-        if updated_user is None:
-            return jsonify({'message': 'User not found'}), 404
-        return jsonify(updated_user),200
-    except Exception as e:
-        return jsonify({'message': str(e)}), 500
 
 # delete user by id
 # @app.delete('/api/del/<from>')
